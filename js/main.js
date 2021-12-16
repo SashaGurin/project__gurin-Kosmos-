932150b0e186be7adbde15442ba7e272f9adef1e
Sasha <55543771+SashaGurin@users.noreply.github.com>
2021-12-16 19:54:38 +0300

    сверстал декстоп

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow-left" src="../img/slider-left.png" alt="arrow">',
+        nextArrow: '<img class="slider__arrow-right" src="../img/slider-right.png" alt="arrow">',
+    });
+
+});
